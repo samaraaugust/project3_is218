3769e4b8659f019e5309b98194dbcb05f2d3bd59
samaraaugust <samaraaugust123@gmail.com>
2022-04-24 18:35:22 -0400

    added form upload to upload html

--- app/songs/__init__.py
+++ app/songs/__init__.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+import csv
+from app.songs.forms import csv_upload
 
 songs = Blueprint('songs', __name__,
                         template_folder='templates')
 @songs.route('/songs')
 def upload_playlist():
-    return render_template('upload_form.html')
+    form = csv_upload()
+    return render_template('upload_form.html', form=form)
